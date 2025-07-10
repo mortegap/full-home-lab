42f9824654bfa7ad4a454cf3a367a650c738e29d
mortegap <miguelortega098@gmail.com>
2025-07-10 13:07:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## WHOLE PERSONAL HOME LAB
+# 
+# Apps in repo:
+- AdGuard
+- Arrs
+- Jellyfin
+- file-browser
+- netalertx
+- Nginx Proxy Manager
+- wireguard
+
+# How To:
+
+```
+# Edit .env and run profiles
+docker compose --profiles arrs up -d
+docker compose --profiles jellyfin up -d
+docker compose --profiles netalertx up -d
+```
---
+++ file-hardlinker/qbit-hardlinker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -x
+
+torrentName=$1
+torrentPath=$2
+torrentCategory=$3
+
+srcDir="/downloads/complete"
+destDir="/downloads/peliculas"
+
+excludedCategories="radarr,tv-sonarr,lidarr,readarr,Uncategorized"
+
+logDir="$(dirname "$0")"
+
+if [[ -z "$torrentCategory" ]]; then
+   torrentCategory="Uncategorized"
+fi
+
+if [[ ",$excludedCategories," == *",$torrentCategory,"* ]]; then
+  echo "[!] Skipped \"${torrentName}\" has excluded \"${torrentCategory}\" category set" >> "$logDir/qbit-hardlinker.log"
+  exit 0
+fi
+
+label="${torrentPath#$srcDir}"
+
+srcPath="${torrentPath}/${torrentName}"
+destPath="${destDir}/${torrentName}"
+
+cp -vlR "${srcPath}" "${destPath}" >> "$logDir/qbit-hardlinker.log"
+if [ $? -eq 0 ]; then
+    echo "[...] Successfully hardlinked \"${torrentName}\" in \"${destPath}\"" >> "$logDir/qbit-hardlinker.log" 
+else
+    echo "Hardlink failed, check logs."
+fi 
